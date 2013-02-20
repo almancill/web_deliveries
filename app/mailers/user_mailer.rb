489b14bf0daa3aa759dff5fc4acde42fe69efc8a
Danilo Antonio Garcia Sales <danilogarcia024@gmail.com>
2013-02-20 13:46:46 -0500

    User Mailer Added

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.sign_up.subject
+  #
+  def sign_up
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
