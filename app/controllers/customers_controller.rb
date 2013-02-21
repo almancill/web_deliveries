3360312d404839c443e26819aa58e8d8f5d7a40f
Danilo Antonio Garcia Sales <danilogarcia024@gmail.com>
2013-02-21 15:34:49 -0500

    Customer controlle functions added

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,35 @@
 class CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def new
+    @customer = Customer.new
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to customers_path, notice: 'Usuario Creado Satisfactoriamente'
+    else
+      render 'new'
+    end 
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    if @customer.update_attributes(customer_params)
+      redirect_to customers_path, notice: 'Usuario Editado Satisfactoriamente'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name, :email)
   end
 end
