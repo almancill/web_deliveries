329dc6cd9bd728aea78dfe41101681057abf0a0f
Danilo Antonio Garcia Sales <danilogarcia024@gmail.com>
2013-06-27 19:51:31 -0500

    Configurations of e-mail added

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app16575779@heroku.com',
+  :password => 'uowg5lo7',
+  :domain => 'domiflash.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the rails application
 WebDeliveries::Application.initialize!
