3a7cec5e3178b1e4dbb25231e039549809714bb1
Danilo Antonio Garcia Sales <danilogarcia024@gmail.com>
2013-07-04 06:44:51 -0500

    Some changes added to Production Environment in Reports Controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,10 +5,18 @@ class ReportsController < ApplicationController
   def obtain
   	date = params[:date]
   	@date = date.split('-')
-  	@number_deliveries = Delivery.where("STRFTIME('%d', created_at) = ? AND STRFTIME('%m', created_at) = ? AND STRFTIME('%Y', created_at) = ?",@date[0],@date[1],@date[2]).count
-  	@total_cost_deliveries = Delivery.where("STRFTIME('%d', created_at) = ? AND STRFTIME('%m', created_at) = ? AND STRFTIME('%Y', created_at) = ?",@date[0],@date[1],@date[2]).sum('delivery_cost')
-  	@total_invoice = Delivery.where("STRFTIME('%d', created_at) = ? AND STRFTIME('%m', created_at) = ? AND STRFTIME('%Y', created_at) = ?",@date[0],@date[1],@date[2]).sum('invoice_cost')
-  	@number_customers = Customer.count
+  	
+    #For Using in Development Environment
+    #@number_deliveries = Delivery.where("STRFTIME('%d', created_at) = ? AND STRFTIME('%m', created_at) = ? AND STRFTIME('%Y', created_at) = ?",@date[0],@date[1],@date[2]).count
+  	#@total_cost_deliveries = Delivery.where("STRFTIME('%d', created_at) = ? AND STRFTIME('%m', created_at) = ? AND STRFTIME('%Y', created_at) = ?",@date[0],@date[1],@date[2]).sum('delivery_cost')
+  	#@total_invoice = Delivery.where("STRFTIME('%d', created_at) = ? AND STRFTIME('%m', created_at) = ? AND STRFTIME('%Y', created_at) = ?",@date[0],@date[1],@date[2]).sum('invoice_cost')
+  	
+    #For Using In Production Environment
+    @number_deliveries = Delivery.where("to_char(created_at,'%d') = ? AND to_char(created_at,'%m') = ? AND to_char(created_at,'%Y') = ?",@date[0],@date[1],@date[2]).count
+    @total_cost_deliveries = Delivery.where("to_char(created_at,'%d') = ? AND to_char(created_at,'%m') = ? AND to_char(created_at,'%Y') = ?",@date[0],@date[1],@date[2]).sum('delivery_cost')
+    @total_invoice = Delivery.where("to_char(created_at,'%d') = ? AND to_char(created_at,'%m') = ? AND to_char(created_at,'%Y') = ?",@date[0],@date[1],@date[2]).sum('invoice_cost')
+    
+    @number_customers = Customer.count
   	@motorcycles = Motorcycle.all
 
     @motorcycles.map do |m|
